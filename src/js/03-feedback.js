e4176407bb07ece3d92fa0cee31a7cdfc4ec7ad5
dimaniagara <ds.sugak@gmail.com>
2022-06-16 03:36:00 -0400

    Home Work 8_2

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,34 +1,37 @@
 import throttle from 'lodash.throttle';
 
 const STORAGE_KEY = 'feedback-form-state';
+const data = JSON.parse(localStorage.getItem(STORAGE_KEY)) || {};
 
 const formEl = document.querySelector('.feedback-form');
-const inputEl = document.querySelector("input[name = 'email']");
-const texareaEl = document.querySelector("textarea[name = 'message']");
-const btnEl = document.querySelector('button[type = "submit"]');
+// const inputEl = document.querySelector("input[name = 'email']");
+// const texareaEl = document.querySelector("textarea[name = 'message']");
+// const btnEl = document.querySelector('button[type = "submit"]');
 
 formEl.addEventListener('input', throttle(onTyping, 500));
 formEl.addEventListener('submit', onSubmit);
 dataRecovery();
 
-function onTyping() {
-  const data = { email: inputEl.value, message: texareaEl.value };
-
+function onTyping(event) {
+  // const data = { email: inputEl.value, message: texareaEl.value };
+  data[event.target.name] = event.target.value;
   localStorage.setItem(STORAGE_KEY, JSON.stringify(data));
 }
 
 function dataRecovery() {
-  const data = localStorage.getItem(STORAGE_KEY);
-  const parsedData = JSON.parse(data);
-  if (data) {
-    inputEl.value = parsedData.email;
-    texareaEl.value = parsedData.message;
+  const parsedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+  if (parsedData) {
+    // inputEl.value = parsedData.email;
+    // texareaEl.value = parsedData.message;
+    const [email, message] = formEl.elements;
+    email.value = parsedData.email || '';
+    message.value = parsedData.message || '';
   }
 }
 
 function onSubmit(e) {
   e.preventDefault();
-  console.log({ email: inputEl.value, message: texareaEl.value });
+  console.log(data);
   e.currentTarget.reset();
 
   localStorage.removeItem(STORAGE_KEY);
